d815f738e380e246c69a2f0d0ceb867dd5cce6ae
nsvrdev <78611170+nsvrdev@users.noreply.github.com>
2021-06-02 13:04:15 +0300

    Initial Commit

---
+++ ASP.NET CORE Web API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_Web_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContex _contex;
+        public ProductsController(StoreContex context)
+        {
+            _contex = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _contex.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _contex.Products.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContex : DbContext
+    {
+        public StoreContex(DbContextOptions<StoreContex> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
